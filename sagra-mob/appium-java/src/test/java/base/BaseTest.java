8746e4ee2bfb811000d6255e9ede0a135b451b34
berlene <berlenebernabe12@gmail.com>
2026-06-22 21:26:50 +0800

    Appium Java Files

--- sagra-mob/appium-java/src/test/java/base/BaseTest.java
+++ sagra-mob/appium-java/src/test/java/base/BaseTest.java
@@ -1,4 +1,37 @@
 package base;
 
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import java.net.URL;
+
 public class BaseTest {
+
+    protected AndroidDriver driver;
+
+    public void setup() throws Exception {
+
+        DesiredCapabilities caps =
+                new DesiredCapabilities();
+
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("deviceName", "Android Emulator");
+
+        caps.setCapability(
+                "appPackage",
+                "com.sagradago.sagramob"
+        );
+
+        caps.setCapability(
+                "appActivity",
+                "com.sagradago.sagramob.MainActivity"
+        );
+
+        driver = new AndroidDriver(
+                new URL("http://127.0.0.1:4723"),
+                caps
+        );
+    }
 }
--- sagra-mob/appium-java/src/test/java/tests/LoginTest.java
+++ sagra-mob/appium-java/src/test/java/tests/LoginTest.java
@@ -1,4 +1,19 @@
 package tests;
 
-public class LoginTest {
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void connectToApp() throws Exception {
+
+        setup();
+
+        System.out.println("Connected to SagradaGoMobile");
+
+        Thread.sleep(5000);
+
+        driver.quit();
+    }
 }
